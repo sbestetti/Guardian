84a6388ea28df8500ce74386b56fbb1f34236920
Sergio Bestetti <sbestetti@gmail.com>
2016-10-19 22:45:22 +0100

    Student management screen

--- BusinessUnit/src/com/guardian/business/dao/StudentDao.java
+++ BusinessUnit/src/com/guardian/business/dao/StudentDao.java
@@ -1,11 +1,34 @@
 package com.guardian.business.dao;
 
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 
+import com.guardian.business.model.Student;
+
+@Named
+@RequestScoped
 public class StudentDao {
 	
 	@PersistenceContext
 	EntityManager em;
+	
+	@Transactional
+	public int addStudent(Student student) {
+		em.persist(student);
+		return 0;
+	}
+	
+	public List<Student> getStudentByName(String name) {
+		String jpql = "SELECT s FROM students s WHERE name LIKE :pname";
+		TypedQuery<Student> query = em.createQuery(jpql, Student.class);
+		query.setParameter("pname", "%" + name + "%");
+		return query.getResultList();
+	}
 
 }
---
+++ BusinessUnit/src/com/guardian/business/mb/StudentsBean.java
@@ -0,0 +1,95 @@
+package com.guardian.business.mb;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+
+import com.guardian.business.dao.StudentDao;
+import com.guardian.business.model.Student;
+
+@ManagedBean
+@ViewScoped
+public class StudentsBean {
+	
+	@Inject
+	private StudentDao dao;
+		
+	private String nameToSearch;
+	private List<Student> searchResults;
+	private Boolean showResults = false;
+	private Boolean showEditForm = false;
+	private Boolean disableEditFields = true;
+	private Student studentToEdit;
+	
+	public void updateResults() {
+		searchResults = dao.getStudentByName(nameToSearch);
+		showResults = true;		
+	}
+	
+	public void editStudent(Student student) {
+		studentToEdit = student;
+		showEditForm = true;
+	}
+	
+	public void enableEditFields() {
+		disableEditFields = false;
+	}
+	
+	
+	
+	//Getters & Setters
+	public List<Student> getStudentsList() {
+		return dao.getStudentByName(nameToSearch);
+	}
+
+	public String getNameToSearch() {
+		return nameToSearch;
+	}
+
+	public void setNameToSearch(String nameToSearch) {
+		this.nameToSearch = nameToSearch;
+	}
+
+	public List<Student> getSearchResults() {
+		return searchResults;
+	}
+
+	public void setSearchResults(List<Student> searchResults) {
+		this.searchResults = searchResults;
+	}
+
+	public Boolean getShowResults() {
+		return showResults;
+	}
+
+	public void setShowResults(Boolean showResults) {
+		this.showResults = showResults;
+	}
+
+	public Boolean getShowEditForm() {
+		return showEditForm;
+	}
+
+	public void setShowEditForm(Boolean showEditForm) {
+		this.showEditForm = showEditForm;
+	}
+
+	public Student getStudentToEdit() {
+		return studentToEdit;
+	}
+
+	public void setStudentToEdit(Student studentToEdit) {
+		this.studentToEdit = studentToEdit;
+	}
+
+	public Boolean getDisableEditFields() {
+		return disableEditFields;
+	}
+
+	public void setDisableEditFields(Boolean disableEditFields) {
+		this.disableEditFields = disableEditFields;
+	}	
+
+}
